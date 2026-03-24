864348c5d05a4c9ddf3c27c90d3deea9f8c35c63
takosuke-88 <artofspider2029@gmail.com>
2026-03-24 20:07:45 +0900

    バグフィックス

--- src/pages/columns/hanahana-losing-patterns.tsx
+++ src/pages/columns/hanahana-losing-patterns.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import { Link } from "react-router-dom";
 import ColumnNavigation from "../../components/ColumnNavigation";
 
 const HanahanaLosingPatternsColumn = () => {
